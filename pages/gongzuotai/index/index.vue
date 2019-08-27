49e5a0e5003179adc86e1dc00f43a81a73be15fa
ysy1989mvp <18214596741@163.com>
2019-08-27 22:11:49 +0800

    init1

---
+++ pages/gongzuotai/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="container">
+		<view class="content">
+			<!-- 顶部是管理员操作选项，权限足够才可以显示 -->
+			<!-- 项目列表九宫格 -->
+			<view>项目管理</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/renwu/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="container">
+		<view class="content">
+			
+		</view>
+	</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
